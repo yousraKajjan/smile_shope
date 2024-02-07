a76d3d307465eb8c24782cc0410d9e7952a6b402
maramkhateeb22 <marookhateeb@gmail.com>
2024-02-07 12:24:04 +0300

    i am maram i add five file

---
+++ lib/screens/five/five.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class five extends StatelessWidget {
+  const five({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
